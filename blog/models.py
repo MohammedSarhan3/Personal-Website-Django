53b2277b02b62ace6905e3d2ea529891b993ff4d
pythone-developer <saraheeeen@gmail.com>
2022-08-31 11:52:37 +0200

    Desgin Models for Posts and blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='posts/')
+    puplish_date = models.DateTimeField(default=timezone.now)
+    content = models.TextField(max_length=5000)
+
+    def __str__(self):
+        return self.title
--- settings/models.py
+++ settings/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class About(models.Model):
+    BlogName = models.CharField(max_length=100)
+    image =models.ImageField(upload_to='about')
+    subtitle= models.TextField(max_length=500)
+    aboutme= models.TextField(max_length=10000)
+    tw_account =models.URLField()
+    fb_account =models.URLField()
+    ins_account =models.URLField()
+    link_account =models.URLField()
